e1db94a8256d8f73d2c7f5b05ba64d4923bbe0fd
Emily Cheyne <emily@bigbearsoftware.ca>
2021-01-08 08:22:47 -0700

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "",     // CognitoClientID
-  "api_base_url": "",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "",                   // CognitoDomainName
-  "redirect_url": ""                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "7ek6lqaid9scvnmu5ib3qalff4",     // CognitoClientID
+  "api_base_url": "https://x6zzs1675f.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-emily-serverless-arc.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d12h0ed8gzqli.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
